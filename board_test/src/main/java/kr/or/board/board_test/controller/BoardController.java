ba1924d049c8dff33fd11313e49596b4a42d0bf7
kim <kim@118.43.39.195>
2020-03-29 21:44:07 +0900

    [게시판 만들어보기 8일차]
    boardList 옵션(10, 20, 30) 선택시 , 선택한 만큼 게시물이 보이게 함.
    기본으로는 20개가 보이도록 해둠.
    
    옵션 선택시 listNum이 url에 붙어 컨트롤러에 진입하도록 함.
    리스트를 불러오는 메서드는 메서드 오버로딩을 통해 값에 맞게 실행되도록 하였음.
    
    페이징은 여전히 아직.

--- board_test/src/main/java/kr/or/board/board_test/controller/BoardController.java
+++ board_test/src/main/java/kr/or/board/board_test/controller/BoardController.java
@@ -18,32 +18,42 @@ public class BoardController {
 	private BoardService boardService;
 
 	@GetMapping("/boardList")
-	public String boardList(@RequestParam(value = "pageNum", required = false)String pageNum ,Model model) {
+	public String boardList(@RequestParam(value = "pageNum", required = false) String pageNum,
+			@RequestParam(value = "listNum", required = false) String listNum, Model model) {
 		System.out.println(pageNum + " <== pageNum");
-		model.addAttribute("board", boardService.getBoardList());
+		System.out.println(listNum + " <== listNum");
+		model.addAttribute("pageNum", pageNum);
+		model.addAttribute("listNum", listNum);
+
+		if (pageNum == null && listNum == null) {
+			model.addAttribute("board", boardService.getBoardList());
+		} else {
+			model.addAttribute("board", boardService.getBoardList(pageNum, listNum));
+		}
 		return "board/boardList";
 	}
-	
+
 	@GetMapping("/boardContent")
 	public String boardContent(@RequestParam(value = "bCode") String bCode, Model model) {
 		model.addAttribute("content", boardService.getBoardContent(bCode));
 		return "board/boardContent";
 	}
-	
+
 	@PostMapping("/contentDel")
-	public @ResponseBody String contentDel(@RequestParam(value="bCode") String bCode) {
+	public @ResponseBody String contentDel(@RequestParam(value = "bCode") String bCode) {
 		boardService.delContent(bCode);
 		return "게시글이 삭제되었습니다.";
 	}
-	
+
 	@GetMapping("/boardWirte")
 	public String boardWirte() {
 		return "board/boardWirte";
 	}
-	
+
 	@PostMapping("/boardWirte")
 	public String contentInsert(Board board) {
 		boardService.contentWrite(board);
 		return "redirect:boardList";
 	}
+
 }
--- board_test/src/main/java/kr/or/board/board_test/service/BoardService.java
+++ board_test/src/main/java/kr/or/board/board_test/service/BoardService.java
@@ -47,6 +47,35 @@ public class BoardService {
 		return bl;
 	}
 
+	// 보드 리스트 전체를 가져온다. (pageNum, listNum)
+	public List<Board> getBoardList(String pageNum, String listNum) {
+		List<Board> bl = boardMapper.getBoardList(pageNum, listNum);
+		String date = "";
+		SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date ymdTime = new Date();
+		String today = format1.format(ymdTime).substring(0, 10); // ex 2020-03-03 현재날짜를 뽑을것.
+
+		String ymd = ""; // 연, 월, 일이 저장될곳
+		String time = ""; // 시간이 저장될 곳
+		String setTime = ""; // ex > 03:00 까지만 저장
+		for (int i = 0; i < bl.size(); i++) { // 가져온 리스트만큼 반복
+
+			date = bl.get(i).getbDate(); // i번째 게시글의 날짜만 빼옴
+
+			ymd = date.substring(0, date.indexOf(" ")); // 가져온 날짜에서 0~공백까지의 글자를 ymd에 저장함
+			time = date.substring(date.indexOf(" "), date.length()); // 가져온 날짜에서 공백~문자열의 끝까지를 time에 저장
+
+			if (today.equals(ymd)) { // 저장한 값이 오늘날짜와 같으면
+				setTime = time.substring(0, 6);
+				bl.get(i).setbDate(setTime); // 시간만 출력하기
+			} else { // 저장한 값이 오늘날짜와 다르면
+				bl.get(i).setbDate(ymd); // 연월일 출력하기
+			}
+		}
+		return bl;
+
+	}
+
 	// 보드 게시글 한개를 가져온다.
 	public Board getBoardContent(String bCode) {
 		return boardMapper.getBoardContent(bCode);
@@ -58,18 +87,17 @@ public class BoardService {
 	}
 
 	// 게시글 등록하기.
-	
 	public void contentWrite(Board board) {
-		if(board.getbPw() != null) { // 비회원의 경우 게시판 등록시 비밀번호를 적어야한다.
+		if (board.getbPw() != null) { // 비회원의 경우 게시판 등록시 비밀번호를 적어야한다.
 			InetAddress local;
 			try {
 				local = InetAddress.getLocalHost();
 				String ip = local.getHostAddress();
 				String[] array = ip.split("\\.");
-				String nickIp = " ("+array[0]+"."+array[1]+")";
+				String nickIp = " (" + array[0] + "." + array[1] + ")";
 				String nick = board.getbNick();
-				board.setbNick(nick+nickIp);
-			}catch (UnknownHostException e) {
+				board.setbNick(nick + nickIp);
+			} catch (UnknownHostException e) {
 				e.printStackTrace();
 			}
 		}
